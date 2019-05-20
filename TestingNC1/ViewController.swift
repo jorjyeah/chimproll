f01f1acf0b5c2f0e866c0a66b82f2a0a5f737fd3
George Joseph Kristian <jorji.gjk@gmail.com>
2019-05-20 13:11:06 +0700

    preparing for reset concept

--- TestingNC1/ViewController.swift
+++ TestingNC1/ViewController.swift
@@ -10,11 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var objectTarget: UIView!
+    var initialCenter = CGPoint()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func objectControllerSwiped(_ gestureRecognizer : UIPanGestureRecognizer) {
+        let translation = gestureRecognizer.translation(in: view)
+        var isRight = true
+        switch gestureRecognizer.state {
+        case .began, .changed:
+            print(translation.x)
+            translation.x < 0 ? (isRight = false) :
+                (isRight = true)
+            objectTarget.moveObject(isRight: isRight)
+        case .ended:
+            break
+        default:
+            break
+        }
+    }
+}
 
+extension UIView {
+    func moveObject(isRight: Bool) {
+        UIView.animate(withDuration: 0, animations: {
+            isRight ? (self.center.x += 1) : (self.center.x -= 1)
+        })
+    }
 }
 
